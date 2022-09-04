cfd6fe8c69da5d2c64dee2e406accf260cf3406d
JojoSwims <johan.boscher@mail.mcgill.ca>
2022-09-04 15:43:46 -0400

    Initial commit of the backend application

---
+++ swimPractice/SwimPractice-Backend/src/main/java/ca/mcgill/ecse321/swimpractice/SwimPracticeApplication.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.swimpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class SwimPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SwimPracticeApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
+}
---
+++ swimPractice/SwimPractice-Backend/src/test/java/ca/mcgill/ecse321/swimpractice/SwimPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.swimpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
